1710f167175c208aceef1e37c2294569a7435964
Maria Keating <maria.keating@posteo.net>
2020-08-17 18:32:57 +0800

    Further optimisations.
    1. Uses svg for favicon.
    2. Crops the minecraft page image.
    3. Moves the css into before_body.html

--- generate.sh
+++ generate.sh
@@ -25,7 +25,7 @@ MakePost()
     OutputFileName=$(GetPostFile "$FilePath")
     OutputFilePath="archive/$OutputFileName"
     lowdown -Thtml --html-no-head-ids -o "$OutputFilePath" "$FilePath"
-    echo "<h4>~Maria Keating</h4>" >> "$OutputFilePath"
+    echo "<p><b>~Maria Keating</b></p>" >> "$OutputFilePath"
     PostTitle="$(awk -vRS="</h1>" '/<h1>/{gsub(/.*<h1>|\n+/,"");print;exit}' "$OutputFilePath")"
     PostDate="$(echo "$FilePath" | grep -o '[0-9]\+')"
     AppendArchive "<li><a href=\"$OutputFilePath\">$PostDate / $PostTitle</a></li>"
@@ -42,7 +42,7 @@ AppendArchive "<h1>Archive</h1>"
 AppendArchive "<ul>"
 ls -1 sources/*.md | sort -r | while read FileName; do MakePost "$FileName"; done
 AppendArchive "</ul>"
-AppendIndex "<h4>Latest Post</h4>"
+AppendIndex "<p><b>Latest Post</b></p>"
 AppendIndex "<hr>"
 LatestPost=$(ls -1 sources/*.md | sort | tail -n1)
 LatestPost="archive/$(GetPostFile "$LatestPost")"
